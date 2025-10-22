ce6301eca79d02da4559b1703678d406fa71d426
Taye Abidakun <tayeabidakun@gmail.com>
2025-10-22 18:43:41 +0100

    division function added to index.py. BMI task given in assignment file

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 print("Welcome to my calculator")
 def calculator():
-    menu = input("1. Addition 2. Subtraction 3. Multiplication")
+    menu = input("1. Addition 2. Subtraction 3. Multiplication 4. Division")
     if(menu == "1"):
         addition()
     elif(menu == "2"):
         subtraction()
     elif(menu == "3"):
         multiplication()
+    elif(menu == "4"):
+        division()
     else:
         print("incorrect entry")
     
@@ -35,4 +37,9 @@ def multiplication():
     result = first * second
     print(result)
 
+def division():
+    first = float(input("Enter the first number: "))
+    second = float(input("Enter the second number: "))
+    result = first / second
+    print(result)
 calculator()
